db0a3cfa1d3dda463db54a4568640762b1a978d3
Kochkin Artem <tomsun86@gmail.com>
2020-08-19 20:06:11 +0300

    add trash icon

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,12 @@
-let closeBtn = document.querySelector('.popup__close-button');
-let editBtn = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup__container');
-let inputName = document.querySelector('.popup__input_type_name');
-let inputProf = document.querySelector('.popup__input_type_prof');
-let name = document.querySelector('.profile__title');
-let prof = document.querySelector('.profile__subtitle');
-let form = document.querySelector('.popup__form');
+const closeBtn = document.querySelector('.popup__close-button');
+const editBtn = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup__container');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputProf = document.querySelector('.popup__input_type_prof');
+const name = document.querySelector('.profile__title');
+const prof = document.querySelector('.profile__subtitle');
+const form = document.querySelector('.popup__form');
+const addCardBtn = document.querySelector('.profile__add-button');
 
 function modalOpened() {
   inputName.value = name.textContent;
@@ -27,3 +28,61 @@ function saveForm(e) {
 editBtn.addEventListener('click', modalOpened);
 closeBtn.addEventListener('click', modalClose);
 form.addEventListener('submit', saveForm);
+
+/**------------------------ */
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+// const initialCards1 = [
+//   'Архыз',
+//   'Челябинская область',
+//   'Иваново',
+//   'Камчатка',
+//   'Холмогорский район',
+//   'Байкал',
+// ];
+
+// function cardsRender() {
+//   const itemContainer = document.querySelector('.cards');
+//   const itemTemplate = document
+//     .querySelector('#itemTemplate')
+//     .content.cloneNode(true);
+//   initialCards.forEach(function (el) {
+//     const cardName = el.name;
+//     const cardLink = el.link;
+//     itemContainer.append(cardName, cardLink);
+//   });
+// }
+// cardsRender();
+// initialCards1.forEach(addItemContainer);
