d76ac532006e8474f2bfa06096c454cede579636
Kochkin Artem <tomsun86@gmail.com>
2020-10-14 00:18:37 +0300

    pr-8_fix1

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,9 +2,6 @@ import Popup from './Popup.js';
 import { popupImages, popupText } from '../utils/constants.js';
 
 export default class PopupWithImage extends Popup {
-  constructor(popupSelector) {
-    super(popupSelector);
-  }
   open(name, link) {
     super.open();
 
